76f840c9628f3083befbab91ef8277e4cf37813d
Kamalka Fernando <rathnaweerakamalka@gmail.com>
2023-02-20 19:19:32 +0530

    First commit

---
+++ MyFirstConsoleApp/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+
+namespace MyFirstConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        
+    }
+}
+
